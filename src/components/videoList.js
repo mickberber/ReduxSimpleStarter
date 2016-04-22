8ce93f9ebbcd577f977dcf637284e5f8b19ce0b3
Michael Berber <mickberber@icloud.com>
2016-04-22 13:25:08 -0700

    develop list items

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import VideoListItem from './videoListItem';
 
 const VideoList = (props) => {
-	return (<ul className="col-md-4 list-group">
-		         {props.videos.length}
-					</ul>);
+  const videoItems = props.videos.map((video) => {
+  	  return <VideoListItem video={video}/>
+  	})
+	return (
+		<ul className="col-md-4 list-group">
+		  {videoItems}
+		</ul>
+	);
 }
 
 export default VideoList;
