8f747917266e81ceb188801c767a8fd5c20779fc
Michael Berber <mickberber@icloud.com>
2016-04-22 11:18:26 -0700

    add state to seach bar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return (<input />);
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+	render() {
+		return (<input onChange={event => console.log(event.target.value)} />)
+	}
 }
 
 export default SearchBar;
